0c746fef55358cebbf1074760da2a21521f18451
Raylson Pereira de Novais Silva <raylson11@gmail.com>
2024-04-01 07:53:09 -0300

    Commit inicial

--- src/main/java/br/com/desafio/sangiorgio/domain/services/PagamentoService.java
+++ src/main/java/br/com/desafio/sangiorgio/domain/services/PagamentoService.java
@@ -5,8 +5,10 @@ import br.com.desafio.sangiorgio.application.enums.StatusPagamentoEnum;
 import br.com.desafio.sangiorgio.application.excption.BusinessException;
 import br.com.desafio.sangiorgio.application.payloads.requests.PagamentoRequest;
 import br.com.desafio.sangiorgio.application.payloads.requests.PedidoRequest;
+import br.com.desafio.sangiorgio.domain.model.Cobranca;
 import br.com.desafio.sangiorgio.domain.model.Pagamento;
 import br.com.desafio.sangiorgio.domain.model.Vendedor;
+import br.com.desafio.sangiorgio.infrastructure.repositories.CobrancaRepository;
 import br.com.desafio.sangiorgio.infrastructure.repositories.PagamentoRepository;
 import br.com.desafio.sangiorgio.infrastructure.repositories.VendedorRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -22,11 +24,13 @@ public class PagamentoService {
     private final ApplicationEventPublisher eventPublisher;
     private final PagamentoRepository pagamentoRepository;
     private final VendedorRepository vendedorRepository;
+    private final CobrancaRepository cobrancaRepository;
 
-    public PagamentoService(ApplicationEventPublisher eventPublisher, PagamentoRepository pagamentoRepository, VendedorRepository vendedorRepository) {
+    public PagamentoService(ApplicationEventPublisher eventPublisher, PagamentoRepository pagamentoRepository, VendedorRepository vendedorRepository, CobrancaRepository cobrancaRepository) {
         this.eventPublisher = eventPublisher;
         this.pagamentoRepository = pagamentoRepository;
         this.vendedorRepository = vendedorRepository;
+        this.cobrancaRepository = cobrancaRepository;
     }
 
     public void create(PedidoRequest pedidoRequest) {
@@ -35,6 +39,9 @@ public class PagamentoService {
 
 
         for (var pagamentoRequest : pedidoRequest.getPagamentos()) {
+            Cobranca cobranca = cobrancaRepository.findById(Long.parseLong(pagamentoRequest.getCodigo()))
+                    .orElseThrow(() -> new BusinessException("Cobrança não encontrada.", "COBRANCA_NAO_ENCONTRADA"));
+
             Pagamento pagamento = processarPagamento(vendedor, pagamentoRequest);
 
             log.info("Creating payment: {}", pagamento);
